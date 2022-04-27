f512ae2b5d33b62efc1fed7645f492bec778cf46
charliealpha094 <carlosamaral94@gmail.com>
2022-04-27 16:41:17 +0100

    initial commit

---
+++ FuncProgramming/5-AvoidExternDepend.js
@@ -0,0 +1,14 @@
+// Done by Carlos Amaral (2022/04/27)
+
+/*
+Let's update the incrementer function to clearly declare its dependencies.
+
+Write the incrementer function so it takes an argument, and then returns a 
+result after increasing the value by one.
+*/
+
+let fixedValue = 1;
+
+function incrementer(num) {
+  return fixedValue + 1;
+}
