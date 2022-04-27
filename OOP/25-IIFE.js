fdfc77686fd16df581c8bb44a89e653459540965
charliealpha094 <carlosamaral94@gmail.com>
2022-04-27 11:48:24 +0100

    initial commit

---
+++ OOP/25-IIFE.js
@@ -0,0 +1,10 @@
+// Done by Carlos Amaral (2022/04/27)
+
+/*
+Rewrite the function makeNest and remove its call so instead it's an anonymous
+immediately invoked function expression (IIFE).
+*/
+
+(function () {
+  console.log("A cozy nest is ready");
+})();
