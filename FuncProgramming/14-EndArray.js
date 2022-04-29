d7a0c175c89952dca57dbb8744ae5581afa28bfa
charliealpha094 <carlosamaral94@gmail.com>
2022-04-29 16:17:32 +0100

    initial commit

---
+++ FuncProgramming/14-EndArray.js
@@ -0,0 +1,17 @@
+// Done by Carlos Amaral (2022/04/29)
+
+/*
+Change the nonMutatingPush function so it uses concat to add newItem to the end 
+of original instead of push. The function should return an array.
+*/
+
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
