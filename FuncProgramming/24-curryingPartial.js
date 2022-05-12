1a476c69939d40442cb8d87bf3ee7321243273c3
charliealpha094 <carlosamaral94@gmail.com>
2022-05-12 14:27:59 +0100

    initial commit

---
+++ FuncProgramming/24-curryingPartial.js
@@ -0,0 +1,15 @@
+// Done by Carlos Amaral (2022/05/12)
+
+/*
+Fill in the body of the add function so it uses currying to add
+parameters x, y, and z.
+*/
+
+function add(x) {
+  // Only change code below this line
+  return (y) => (z) => x + y + z;
+
+  // Only change code above this line
+}
+
+console.log(add(10)(20)(30));
